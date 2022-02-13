e11f22276283799644a81f08f6e3881c0ab4e545
Piyush Ranjan <piyushranjan95@gmail.com>
2022-02-13 13:47:58 +0530

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TestsComponent } from './tests/tests.component';
+import { TestWindowComponent } from './test-window/test-window.component';
+
+const routes: Routes = [
+  { path: 'home', component: TestsComponent },
+  { path: 'test', component: TestWindowComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
